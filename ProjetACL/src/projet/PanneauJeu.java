aa00fad3267757ef061277218bbb9119afbd3422
Sabri Ben Raies <sabri.benrais@gmail.com>
2022-11-10 06:22:28 +0100

    adding FPS display

--- ProjetACL/src/projet/PanneauJeu.java
+++ ProjetACL/src/projet/PanneauJeu.java
@@ -71,9 +71,12 @@ public class PanneauJeu extends JPanel implements Runnable {
 		double delta=0;
 		long lastTime=System.nanoTime();
 		long currentTime;
+		long timer=0;
+		int drawCount=0;
 		while (gameThread != null) {
 			
 			currentTime=System.nanoTime();
+			timer+=(currentTime-lastTime);
 			delta+= (currentTime-lastTime)/drawInterval;
 			lastTime=currentTime;
 			if(delta>=1) {
@@ -82,6 +85,12 @@ public class PanneauJeu extends JPanel implements Runnable {
 				// Draw the screen
 				repaint();
 				delta--;
+				drawCount++;
+			}
+			if(timer>=1000000000) {
+				System.out.println("FPS:"+drawCount);
+				drawCount=0;
+				timer=0;
 			}
 		}
 
