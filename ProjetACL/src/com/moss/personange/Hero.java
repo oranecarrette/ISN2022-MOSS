51a185edea7574affe9694e9dfaea8514b783a1f
Sabri Ben Raies <sabri.benrais@gmail.com>
2022-11-11 12:37:02 +0100

    Màj gitignore

--- ProjetACL/src/com/moss/personange/Hero.java
+++ ProjetACL/src/com/moss/personange/Hero.java
@@ -77,26 +77,8 @@ public class Hero extends Personnage {
 			break;
 		}
 		g2.drawImage(image, x, y, pan.tileSize, pan.tileSize, null);
-
-		// g2.fillRect(x,y, pan.tileSize, pan.tileSize);
+		
 	}
 
-	/*
-	 * public Hero() { //constructeur par defaut this.posX=0; this.posY=0; }
-	 * 
-	 * public Hero(int posX,int posY) { //constructeur avec attributs renseignes
-	 * this.posX=posX; this.posY=posY; }
-	 * 
-	 * public String toString() { return "posx= "+posX+"\tposY= "+posY; //\t =
-	 * tabulations }
-	 * 
-	 * public void moveUp() { posY--; }
-	 * 
-	 * public void moveDown() { posY++; }
-	 * 
-	 * public void moveRight() { posX++; }
-	 * 
-	 * public void moveLeft() { posX--; }
-	 */
 
 }
