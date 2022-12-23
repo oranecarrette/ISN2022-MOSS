eb19c77e417ae3f13a3f4aee6cb6fb5c5badaac9
Orane <carretteorane@gmail.com>
2022-12-23 12:22:48 +0100

    les objets changent de position initiale en fct du maze

--- ProjetACL/src/com/moss/object/Key.java
+++ ProjetACL/src/com/moss/object/Key.java
@@ -5,21 +5,45 @@ import java.io.IOException;
 
 import javax.imageio.ImageIO;
 
+import com.moss.main.GamePanel;
 import com.moss.main.Main;
+import com.moss.maze.Maze;
 
-public class Key extends AllObject {
+public class Key extends Objects {
 	
-	public Key() {
+	GamePanel pan;
+	int mazeNumero = Maze.getNumero();
+	
+	public Key(GamePanel pan) {
+		
+		this.pan = pan;
+		
 		String path;
 		File file;
 		path=Main.currentDir +"/images/treasure/key.png";
 		file = new File(path);
 		name="Key";
+		
+		ListofPosX.add(10);
+		ListofPosX.add(4);
+		ListofPosX.add(14);
+		ListofPosY.add(1);
+		ListofPosY.add(3);
+		ListofPosY.add(10);
+		
+		
 		try {
 			image=ImageIO.read(file);
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
+	}
+		
+
+	public void setKey() {
+		 pan.obj[0]=new Key(pan);
+		 pan.obj[0].x=ListofPosX.get(mazeNumero)*pan.tileSize;
+		 pan.obj[0].y=ListofPosY.get(mazeNumero)*pan.tileSize;
 	}
 
 }
--- ProjetACL/src/com/moss/object/TreasureOpen.java
+++ ProjetACL/src/com/moss/object/TreasureOpen.java
@@ -5,22 +5,43 @@ import java.io.IOException;
 
 import javax.imageio.ImageIO;
 
+import com.moss.main.GamePanel;
 import com.moss.main.Main;
+import com.moss.maze.Maze;
 
-public class TreasureOpen  extends AllObject{
+public class TreasureOpen extends Objects{
 	
-	public TreasureOpen() {
+	GamePanel pan;
+	int mazeNumero = Maze.getNumero();
+	
+	public TreasureOpen(GamePanel pan) {
+		
+		this.pan = pan;
+		
 		String path;
 		File file;
 		path=Main.currentDir +"/images/treasure/treasureOpen.png";
 		file = new File(path);
 		name="Treasure open";
+		
+		ListofPosX.add(2);
+		ListofPosX.add(12);
+		ListofPosX.add(10);
+		ListofPosY.add(8);
+		ListofPosY.add(8);
+		ListofPosY.add(6);
+		
 		try {
 			image=ImageIO.read(file);
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 	}
-
+	
+	public void setTreasureOpen() {
+		 pan.obj[2]=new TreasureOpen(pan);
+		 pan.obj[2].x=ListofPosX.get(mazeNumero)*pan.tileSize;
+		 pan.obj[2].y=ListofPosY.get(mazeNumero)*pan.tileSize;
+	}
 
 }
